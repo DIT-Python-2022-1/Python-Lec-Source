a64679df8881f1950ccb0a35911bba407a49acff
장성익|SeongIk Jang <94339822+cyber-steer@users.noreply.github.com>
2022-03-10 14:26:04 +0900

    cal function ex

---
+++ cal_funtion.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a+b
+def minus(a,b):
+    return a-b
+def multi(a,b):
+    return a*b
+def divide(a,b):
+    return a/b
+
+print(add(1,2))
+print(minus(1,2))
+print(multi(1,2))
+print(divide(1,2))
